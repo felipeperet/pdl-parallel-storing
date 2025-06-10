68b36b578faa21e0200648fa543d9f9266b165c4
Felipe Péret <sasdelli.pi@gmail.com>
2025-06-10 18:58:56 -0300

    Proving second property

--- PdlParallelStoring/Properties.lean
+++ PdlParallelStoring/Properties.lean
@@ -24,3 +24,27 @@ def s₁_comp_r₁ (F : Frame) [Structured F] [P : Proper F] :
     simp [P.s₁, P.r₁]
     use u
     rw [h_eq]
+
+-- Property II) Rs₂ ; Rr₂ = Id
+def s₂_comp_r₂ (F : Frame) [Structured F] [P : Proper F] :
+    ∀ {s u}, Relation.Comp (F.R π.s₂) (F.R π.r₂) s u ↔ (s = u) := by
+  intros s u
+  constructor
+  case mp =>
+    intro hcomp
+    unfold Relation.Comp at hcomp
+    obtain ⟨t, hs₂, hr₂⟩ := hcomp
+    rw [P.s₂] at hs₂
+    rw [P.r₂] at hr₂
+    obtain ⟨s₁, t₁, hs_eq, ht_eq⟩ := hs₂
+    obtain ⟨s₂, t₂, ht_eq₂, hu_eq⟩ := hr₂
+    have h_eq : s₁ ⋆ t₁ = s₂ ⋆ t₂ := by rw [← ht_eq, ht_eq₂]
+    have t₁_eq_t₂ : t₁ = t₂ := (State.inject.mp h_eq).2
+    rw [hs_eq, hu_eq]
+    exact t₁_eq_t₂
+  case mpr =>
+    intro h_eq
+    unfold Relation.Comp
+    use s ⋆ s
+    simp [P.s₂, P.r₂]
+    constructor <;> use u <;> rw [h_eq]
