880cd4d94d8eddcf769bef47358e633d5d09b840
Felipe Péret <sasdelli.pi@gmail.com>
2025-06-27 00:41:02 -0300

    Proving soundness for sameDomain, unicity, and storeRestore axioms

--- PdlParallelStoring/Fragment/System.lean
+++ PdlParallelStoring/Fragment/System.lean
@@ -22,7 +22,7 @@ inductive RSPDL₀ : Φ → Prop where
   | temporal φ : RSPDL₀ (φ → ([π.s₁] ⟨π.r₁⟩ φ))
   | sameDomain : RSPDL₀ ((⟨π.r₁⟩ ⊤) ↔ (⟨π.r₂⟩ ⊤))
   | unicity φ : RSPDL₀ ((⟨π.s₁ ; π.r₁⟩ φ) ↔ ([π.s₁ ; π.r₁] φ))
-  | storeRestoreId φ : RSPDL₀ ([π.s₁ ; π.r₂] φ → φ)
+  | storeRestoreId φ : RSPDL₀ (([π.s₁ ; π.r₂] φ) → φ)
   | storeRestoreDiamond φ : RSPDL₀ (φ → ([π.s₁ ; π.r₂] ⟨π.s₁ ; π.r₂⟩ φ))
   | storeRestoreIterate φ : RSPDL₀ (([π.s₁ ; π.r₂] φ) → ([π.s₁ ; π.r₂] [π.s₁ ; π.r₂] φ))
   -- Inference Rules
@@ -79,10 +79,10 @@ theorem soundness : ∀ {φ : Φ}, (⊢ φ) → (⊨ φ) := by
       exact hTrue
   | functional =>
       intros _ P M _ hEq w hSat
+      subst hEq
       simp [satisfies] at hSat
       obtain ⟨h₁, h₂⟩ := hSat
       obtain ⟨s, hRws, hSat⟩ := h₁
-      subst hEq
       rw [P.r₁] at hRws
       obtain ⟨s₁, s₂ , hEq₁, hEq₂⟩ := hRws
       obtain ⟨s', hRws', hNotSat⟩ := h₂
@@ -95,16 +95,97 @@ theorem soundness : ∀ {φ : Φ}, (⊢ φ) → (⊨ φ) := by
       exact hNotSat hSat
   | temporal φ =>
       intros _ P M _ hEq w hSat
+      subst hEq
       simp [satisfies] at hSat
       obtain ⟨hSat₁, hSat₂⟩ := hSat
       obtain ⟨s, hAnd⟩ := hSat₂
       obtain ⟨hRws, hAll⟩ := hAnd
       have hR₁ : M.F.R π.r₁ s w :=  by
-        subst hEq
         rw [P.s₁] at hRws
         obtain ⟨w', t, hw_eq, hs_eq⟩ := hRws
         rw [P.r₁]
         use w', t
       have hNotSat : ¬ (M,w) ⊨ φ := hAll w hR₁
       exact hNotSat hSat₁
+  | sameDomain =>
+      intros _ P M _ hEq w
+      subst hEq
+      constructor
+      . intros hSat₁
+        obtain ⟨hSat₂, hAll⟩ := hSat₁
+        simp [satisfies] at *
+        obtain ⟨s, hRws⟩ := hSat₂
+        simp [P.r₁] at hRws
+        obtain ⟨s', hw_eq⟩ := hRws
+        have hR₂ : M.F.R π.r₂ w s' := by
+          rw [P.r₂, hw_eq]
+          use s, s'
+        exact hAll s' hR₂
+      . intros hSat₁
+        obtain ⟨hSat₂, hAll⟩ := hSat₁
+        simp [satisfies] at *
+        obtain ⟨t, hRwt⟩ := hSat₂
+        simp [P.r₂] at hRwt
+        obtain ⟨s', ht_eq⟩ := hRwt
+        have hR₁ : M.F.R π.r₁ w s' := by
+          rw [P.r₁, ht_eq]
+          use s', t
+        exact hAll s' hR₁
+  | unicity φ =>
+      intros _ _ M _ _ w
+      constructor
+      . intros hAnd
+        obtain ⟨hSat₁, hSat₂⟩ := hAnd
+        simp [satisfies] at *
+        obtain ⟨s, hRws, hSat₁⟩ := hSat₁
+        obtain ⟨x, hRwx, hNotSat⟩ := hSat₂
+        have hs_eq_w : s = w := by
+          rw [Standard.comp] at hRws
+          rw [s₁_comp_r₁] at hRws
+          exact hRws.symm
+        have hx_eq_w : x = w := by
+          rw [Standard.comp] at hRwx
+          rw [s₁_comp_r₁] at hRwx
+          exact hRwx.symm
+        rw [hs_eq_w] at hSat₁
+        rw [hx_eq_w] at hNotSat
+        exact hNotSat hSat₁
+      . intros hSat
+        simp [satisfies] at *
+        obtain ⟨hPos, hNeg⟩ := hSat
+        have hReach : M.F.R (π.s₁ ; π.r₁) w w := by
+          rw [Standard.comp, s₁_comp_r₁]
+        have hPhiHolds : (M, w) ⊨ φ := hPos w hReach
+        have hPhiNotHolds : ¬(M, w) ⊨ φ := hNeg w hReach
+        exact hPhiNotHolds hPhiHolds
+  | storeRestoreId φ =>
+      intros _ _ M _ _ w hAnd
+      obtain ⟨hSat, hPhiNotHolds⟩ := hAnd
+      simp [satisfies] at hSat
+      have hReach : M.F.R (π.s₁ ; π.r₂) w w := by
+        rw [Standard.comp, s₁_comp_r₂]
+        simp [State.equiv.refl]
+      have hPhiHolds : (M, w) ⊨ φ := hSat w hReach
+      exact hPhiNotHolds hPhiHolds
+  | storeRestoreDiamond φ =>
+      intros _ _ M _ _ w hAnd
+      obtain ⟨hPhiHolds, hSat₂⟩ := hAnd
+      simp [satisfies] at *
+      obtain ⟨s, hRws, hAll⟩ := hSat₂
+      have hReach : M.F.R (π.s₁ ; π.r₂) s w := by
+        rw [Standard.comp, s₁_comp_r₂] at *
+        exact State.equiv.symm hRws
+      have hPhiNotHolds : ¬(M, w) ⊨ φ := hAll w hReach
+      exact hPhiNotHolds hPhiHolds
+  | storeRestoreIterate φ =>
+      intros _ P M _ hEq w hAnd
+      subst hEq
+      obtain ⟨hAll, hSat⟩ := hAnd
+      simp [satisfies] at *
+      obtain ⟨s, hRws, t, hRst, hPhiNotHolds⟩ := hSat
+      have hReach : M.F.R (π.s₁ ; π.r₂) w t := by
+        simp [Standard.comp, s₁_comp_r₂] at *
+        simp [State.equiv.trans hRws hRst]
+      have hPhiHolds : (M, t) ⊨ φ := hAll t hReach
+      exact hPhiNotHolds hPhiHolds
   | _ => sorry
