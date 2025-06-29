b01722c2f2560f04f0542e9234179056e57de6e8
Felipe Péret <sasdelli.pi@gmail.com>
2025-06-29 02:07:27 -0300

    Fixing import list

--- PdlParallelStoring.lean
+++ PdlParallelStoring.lean
@@ -1,8 +1,8 @@
 -- This module serves as the root of the `PdlParallelStoring` library.
 -- Import modules here that should be built as part of the library.
 import PdlParallelStoring.Syntax
+import PdlParallelStoring.Examples
 import PdlParallelStoring.Semantics
 import PdlParallelStoring.Properties
-import PdlParallelStoring.Examples
-
-import PdlParallelStoring.Fragment.System
+import PdlParallelStoring.AxiomaticSystem
+import PdlParallelStoring.Soundness
